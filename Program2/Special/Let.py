f0a477ff72133451b181e4c82b6803c641fbc9a3
Annabelle Kanchirathingal <77216805+annabellekanch@users.noreply.github.com>
2021-11-13 21:11:37 -0600

    Update Let.py

--- Program2/Special/Let.py
+++ Program2/Special/Let.py
@@ -4,6 +4,7 @@ from Tree import Nil
 from Tree import Environment
 from Print import Printer
 from Special import Special
+from Tree import *
 
 class Let(Special):
     def __init__(self):
@@ -11,3 +12,34 @@ class Let(Special):
 
     def print(self, t, n, p):
         Printer.printLet(t, n, p)
+
+    def eval(self, t, env):
+        args = None
+        exp = None
+        local = Environment(env)
+        args = t.getCdr().getCar()
+        exp = t.getCdr().getCdr().getCar()
+        args = self.eval_body(args, local)
+        return exp.eval(local)
+
+    def eval_body(self, t, env):
+        if t is None or t.isNull():
+            list = Cons(Nil(), Nil())
+            return list
+        else:
+            arg = None
+            exp = None
+            rest = None
+            arg = t.getCar().getCar()
+            exp = t.getCar().getCdr().getCar()
+            rest = t.getCdr()
+
+            if arg.isSymbol():
+                env.define(arg, exp.eval(env))
+                return self.eval_body(rest, env)
+            elif arg.isPair():
+                return arg.eval(env)
+            elif arg is None or arg.isNull():
+                return Nil()
+        return None
+
