12625cfe2e54b2272d56abd2b59f9f48d3c75aba
Annabelle Kanchirathingal <77216805+annabellekanch@users.noreply.github.com>
2021-11-13 21:12:37 -0600

    Update Set.py

--- Program2/Special/Set.py
+++ Program2/Special/Set.py
@@ -4,6 +4,7 @@ from Tree import Nil
 #from Tree import Unspecific
 from Print import Printer
 from Special import Special
+from Tree import *
 
 class Set(Special):
     def __init__(self):
@@ -11,3 +12,11 @@ class Set(Special):
     
     def print(self, t, n, p):
         Printer.printSet(t, n, p)
+
+    def eval(self, t, env):
+        id = None
+        exp = None
+        id = t.getCdr().getCar()
+        exp = t.getCdr().getCdr().getCar()
+        env.define(id, exp.eval(env))
+        return StrLit("")
