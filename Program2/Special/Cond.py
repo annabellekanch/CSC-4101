64a5a19b308bbdc6b8165cc32dff24bb3da975f9
Annabelle Kanchirathingal <77216805+annabellekanch@users.noreply.github.com>
2021-11-13 21:07:14 -0600

    Update Cond.py

--- Program2/Special/Cond.py
+++ Program2/Special/Cond.py
@@ -1,6 +1,7 @@
 # Cond -- Parse tree node strategy for printing the special form cond
 
 from Tree import BoolLit
+from Tree import *
 from Tree import Nil
 #from Tree import Unspecific
 from Print import Printer
@@ -12,3 +13,16 @@ class Cond(Special):
 
     def print(self, t, n, p):
         Printer.printCond(t, n, p)
+
+    def eval(self, t, env):
+        exp = None
+        exp = t.getCdr()
+
+        while (not(exp.getCar()).getCar().eval(env).getBoolean()) and ((not exp.isNull())):
+            exp = exp.getCdr()
+
+        if exp.isNull():
+            return Nil()
+        else:
+            return (exp.getCar().getCdr().getCar().eval(env))
+
