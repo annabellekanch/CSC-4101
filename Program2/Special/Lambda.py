29ad31e7e096174b9f8126bc6dbe288b73b4cef0
Annabelle Kanchirathingal <77216805+annabellekanch@users.noreply.github.com>
2021-11-13 21:11:14 -0600

    Update Lambda.py

--- Program2/Special/Lambda.py
+++ Program2/Special/Lambda.py
@@ -1,45 +1,15 @@
-# Let -- Parse tree node strategy for printing the special form let
+# Lambda -- Parse tree node strategy for printing the special form lambda
 
-from Tree import Nil
-from Tree import Environment
+from Tree import Closure
 from Print import Printer
 from Special import Special
-from Tree import *
 
-class Let(Special):
+class Lambda(Special):
     def __init__(self):
         pass
 
     def print(self, t, n, p):
-        Printer.printLet(t, n, p)
-
+        Printer.printLambda(t, n, p)
+        
     def eval(self, t, env):
-        args = None
-        exp = None
-        local = Environment(env)
-        args = t.getCdr().getCar()
-        exp = t.getCdr().getCdr().getCar()
-        args = self.eval_body(args, local)
-        return exp.eval(local)
-
-    def eval_body(self, t, env):
-        if t is None or t.isNull():
-            list = Cons(Nil(), Nil())
-            return list
-        else:
-            arg = None
-            exp = None
-            rest = None
-            arg = t.getCar().getCar()
-            exp = t.getCar().getCdr().getCar()
-            rest = t.getCdr()
-
-            if arg.isSymbol():
-                env.define(arg, exp.eval(env))
-                return self.eval_body(rest, env)
-            elif arg.isPair():
-                return arg.eval(env)
-            elif arg is None or arg.isNull():
-                return Nil()
-        return None
-
+        return Closure(t.getCdr(), env)
