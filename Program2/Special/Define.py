bb02feb87fb6fcdd7805fb90b843f23236cbdc21
Annabelle Kanchirathingal <77216805+annabellekanch@users.noreply.github.com>
2021-11-13 21:07:36 -0600

    Update Define.py

--- Program2/Special/Define.py
+++ Program2/Special/Define.py
@@ -3,6 +3,7 @@
 from Tree import Ident
 from Tree import Nil
 from Tree import Cons
+from Tree import *
 #from Tree import Void
 from Print import Printer
 from Special import Special
@@ -13,3 +14,21 @@ class Define(Special):
 
     def print(self, t, n, p):
         Printer.printDefine(t, n, p)
+        
+    def eval(self, t, env):
+        id = None
+        val = None
+
+        id = t.getCdr().getCar()
+        val = t.getCdr().getCdr().getCar()
+
+        if id.isSymbol():
+            env.define(id, val)
+        else:
+            func = Closure(Cons(t.getCdr().getCar().getCdr(), t.getCdr().getCdr()), env)
+            env.define(id.getCar(), func)
+
+        return StrLit("; no values returned")
+
+    
+
